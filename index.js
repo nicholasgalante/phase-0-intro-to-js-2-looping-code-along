d450502189413c971d1d78696692c7cc05d8610b
Nicholas <nicholasgalante94@gmail.com>
2022-11-23 16:22:59 -0500

    commit tree f24555f540795ee9f2dd92eb4be3156071d9915f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+countDown = (int) => {
+    while (int > -1) {
+        console.log(int);
+        int--
+    }
+}
+
+writeCards = (names, event) => {
+   const newArray = [];
+    for (let index = 0; index < names.length; index++) {
+        newArray[index] = `Thank you, ${names[index]}, for the wonderful ${event} gift!`;
+    }
+    return newArray;
+}
